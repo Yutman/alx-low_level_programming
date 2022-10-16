374ef4474aede804ec0f7d3fda5c102746ba5218
Yutman <kuriapeter95@gmail.com>
2022-10-16 13:39:13 -0700

    I sometimes suffer from insomnia

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+  * main - Program start
+  * Return: Returns 0 when program success
+  */
+int main(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+
+}
