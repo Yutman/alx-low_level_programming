74dceba7faf42545c339d40d589a778ae34c1247
Yutman <kuriapeter95@gmail.com>
2022-10-18 11:13:56 -0700

    Prints putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+include "main.h"
+
+/**
+  * main -prints putchar followed by a newline
+  * Return: Return 0 when successful
+  */
+
+int main(void)
+{
+	int i;
+	char p[] = "_putchar"
+		for (i = 0; i < 8; i++)
+		{
+			_putchar(p[i]);
+		}
+	_putchar ('\n');
+	return (0);
+}
+
