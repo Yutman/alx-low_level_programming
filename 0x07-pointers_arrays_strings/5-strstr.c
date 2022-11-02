c3d65c6363d1986c716512abd1f5a9de98e35a62
Yutman <kuriapeter95@gmail.com>
2022-11-02 11:24:20 -0700

    Function that locates a substring

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -14,8 +14,6 @@ char *_strstr(char *haystack, char *needle)
 
 	while (haystack[i] != '\0')
 	{
-		while (needle[j] != '\0')
-		{
 		if (haystack[i + j] == needle[j])
 			j++;
 		else
@@ -25,4 +23,5 @@ char *_strstr(char *haystack, char *needle)
 		return (haystack + i);
 	i++;
 	}
+return (NULL);
 }
