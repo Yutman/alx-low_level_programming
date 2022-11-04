45f9da8f18e4c6a5f5d811188cb3a8feb719a252
Yutman <kuriapeter95@gmail.com>
2022-11-04 14:03:00 -0700

    Find result of number x raised to power y

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - raises a number x to power
+ *			y
+ * @x: input integer
+ * @y: power it is raised by
+ * Return: result
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	return (_pow_recursion(x, y - 1));
+}
